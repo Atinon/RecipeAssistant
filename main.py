e873468d02f0729e21880042b0141e6ec01a8de5
Nikola <nkl_andonov@yahoo.com>
2024-01-25 02:06:12 +0200

    Isntalled rdflib, it's dependencies and updated requirements.txt; Sample query in main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+from rdflib import Graph, URIRef, Literal
+
+g = Graph()  # Initial empty Graph object
+
+ontology_file_path = "ontology/WhatToMake_Individuals.rdf"
+g.parse(ontology_file_path)
+
+query = """
+PREFIX food: <http://purl.org/heals/food/>
+PREFIX ingredient: <http://purl.org/heals/ingredient/>
+SELECT DISTINCT ?recipe
+WHERE {
+?recipe food:hasIngredient ingredient:Beef .
+}
+"""
+
+results = g.query(query)
+
+for row in results:
+    print(row)
